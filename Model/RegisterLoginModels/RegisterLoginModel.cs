613c902f3bcf6f04063636e7268bf03b811ae416
Alexander Lysenko <alexander.lysenko1@gmail.com>
2019-02-18 15:52:28 +0200

    18-02-2019

---
+++ Model/RegisterLoginModels/RegisterLoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebCustomerApp.Models.AccountViewModels;
+
+namespace WebCustomerApp.Models.RegisterLoginModels
+{
+   public class RegisterLoginModel
+    {
+        public RegisterViewModel RegisterModel { get; set; }
+        public LoginViewModel LoginModel { get; set; }
+    }
+}
